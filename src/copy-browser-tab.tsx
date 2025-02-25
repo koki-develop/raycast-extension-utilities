c34f45c8b6b5b145cc4aedda465d2e616eddff73
koki-develop <kou.pg.0131@gmail.com>
2025-02-25 20:11:58 +0900

    Add Copy Browser Tab command

---
+++ src/copy-browser-tab.tsx
@@ -0,0 +1,57 @@
+import { ActionPanel, List, Action, BrowserExtension } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+
+const tabToMarkdown = (tab: BrowserExtension.Tab) =>
+  `[${tab.title ?? "(untitled)"}](${tab.url})`;
+
+const tabToHtml = (tab: BrowserExtension.Tab) =>
+  `<a href="${tab.url}">${tab.title ?? "(untitled)"}</a>`;
+
+export default function Command() {
+  const { data: tabs, isLoading } = usePromise(async () => {
+    const tabs = await BrowserExtension.getTabs();
+    return tabs.sort((a, b) => {
+      if (a.active) return -1;
+      if (b.active) return 1;
+      return 0;
+    });
+  });
+
+  return (
+    <List isLoading={isLoading}>
+      {tabs?.map((tab) => (
+        <List.Item
+          key={tab.id}
+          title={tab.title ?? "(untitled)"}
+          subtitle={tab.url}
+          icon={tab.favicon}
+          actions={
+            <ActionPanel>
+              <Action.CopyToClipboard
+                title="Copy as Markdown"
+                content={{ text: tabToMarkdown(tab) }}
+              />
+              <Action.CopyToClipboard
+                // eslint-disable-next-line @raycast/prefer-title-case
+                title="Copy as HTML"
+                content={{ text: tabToHtml(tab) }}
+              />
+              <Action.CopyToClipboard
+                title="Copy as Rich Text"
+                content={{ html: tabToHtml(tab) }}
+              />
+              <Action.CopyToClipboard
+                title="Copy Title"
+                content={{ text: tab.title ?? "(untitled)" }}
+              />
+              <Action.CopyToClipboard
+                title="Copy URL"
+                content={{ text: tab.url }}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
